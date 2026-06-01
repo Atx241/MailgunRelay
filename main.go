78fbc6fda969756a1e0419229fd5de249cf3c349
Atx241 <121907198+Atx241@users.noreply.github.com>
2026-06-01 16:46:25 -0600

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ func main() {
 	http.HandleFunc("/post-mime", relay)
 
 	fmt.Println("Initialized Mailgun relay")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 func relay(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("Received message")
