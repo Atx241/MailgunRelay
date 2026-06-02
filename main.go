3e18357d4780f851c3dc9f90d8301b26a7cf5172
Atx241 <121907198+Atx241@users.noreply.github.com>
2026-06-01 20:12:25 -0600

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
+	"net/smtp"
 )
 
 func main() {
@@ -20,11 +20,20 @@ func relay(res http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
 		fmt.Print("[", k, "]: ", v, "\n")
 	}
-	fmt.Println("[BODY]:")
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(string(body))
+	// fmt.Println("[BODY]:")
+	// body, err := io.ReadAll(req.Body)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// fmt.Println(string(body))
+
+	mime := req.FormValue("body-mime")
+	from := req.FormValue("sender")
+	to := req.FormValue("recipient")
+
+	fmt.Println("Sender:", from)
+	fmt.Println("Reciever:", to)
+
+	smtp.SendMail("localhost:25", nil, from, []string{to}, []byte(mime))
 }
